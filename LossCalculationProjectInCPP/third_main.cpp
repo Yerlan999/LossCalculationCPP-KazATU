14c97f159733c0384ebfd5ab410535b480637757
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2022-12-26 01:11:04 +0600

    still columns issue is unsolved

--- LossCalculationProjectInCPP/third_main.cpp
+++ LossCalculationProjectInCPP/third_main.cpp
@@ -55,20 +55,20 @@ int main() {
 	XLDocument doc;
 	doc.open("./Promzona.xlsx");
 	auto wks = doc.workbook().worksheet(sheetNames[0]);
-	auto rng = wks.range(XLCellReference("A2"), XLCellReference("CT3500"));
+	//auto rng = wks.range(XLCellReference("A2"), XLCellReference("CT3500"));
 
-	cout << "Maxx rows count: " << OpenXLSX::MAX_ROWS << endl;
-	cout << "Maxx columns count: " << OpenXLSX::MAX_COLS << endl;
-	cout << "Cell count: " << std::distance(rng.begin(), rng.end()) << endl;
+	//cout << "Maxx rows count: " << OpenXLSX::MAX_ROWS << endl;
+	//cout << "Maxx columns count: " << OpenXLSX::MAX_COLS << endl;
+	//cout << "Cell count: " << std::distance(rng.begin(), rng.end()) << endl;
 
-	//for (auto& row : wks.rows()) {
-	//	for (auto& value : std::deque<xlcellvalue>(row.values())) {
-	//		cout << value << endl;
-	//	}
-	//	if (counter == 5) break;
-	//	cout << endl;
-	//	counter++;
-	//}
+	for (auto& row : wks.rows()) {
+		for (auto& value : std::deque<XLCellValue>(row.values())) {
+			cout << value << endl;
+		}
+		if (counter == 5) break;
+		cout << endl;
+		counter++;
+	}
 
 	//double cell_value = wks.cell("a2").value();
 	//cout << cell_value << endl;
