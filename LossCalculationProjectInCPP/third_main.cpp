534f277aa8696204f49faea56ee5c1e2439ebcfc
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2022-12-31 23:35:22 +0600

    decided to use native array for holding excel doc values

--- LossCalculationProjectInCPP/third_main.cpp
+++ LossCalculationProjectInCPP/third_main.cpp
@@ -4,18 +4,20 @@
 #include <vector>
 #include <chrono>
 #include <sstream>
+#include <array>
 #include <typeinfo>
 #include <complex>
 #include <Eigen/Dense>
 #include <OpenXLSX.hpp>
 
-using namespace std::chrono;
 using namespace std;
+using namespace std::chrono;
 using namespace OpenXLSX;
 
 using Eigen::MatrixXcf;
+using namespace Eigen;
 
-complex<double> mycomplex(10.0, 2.0);
+complex<float> mycomplex(10.0, -2.0);
 
 // Моя функция для конвертации текста в цифру с плав. точкой
 float stringToFloat(string s)
@@ -36,33 +38,37 @@ int stringToInt(string s)
 }
 
 
-MatrixXcf makeMatrix()
-{
-	MatrixXcf m(2, 2);
-	m(0, 0) = mycomplex;
-	m(1, 0) = 2.5;
-	m(0, 1) = -1;
-	m(1, 1) = m(1, 0) + m(0, 1);
-	return m;
-}
 //                         CT        DB        CT        DB        CT        DB 
 string worksheet_names[6] = {"Sheet1", "Sheet2", "Sheet3", "Sheet4", "Sheet5", "Sheet6"};
 int rows_counter = 1;
 int values_counter = 5;
 
+
+
 int main() {
 	
 	auto start = high_resolution_clock::now();
 
-	MatrixXcf my_matrix = makeMatrix();
+	
+	complex<float> matrix[3][50][560] = {};
+	matrix[0][0][0] = mycomplex;
 
-	//cout << my_matrix << endl;
+
+	Matrix <complex<float>, 3, 3 > my_matrix;
+	my_matrix.setZero();
+	my_matrix(1, 2) = mycomplex;
+	
+	
+	std::cout << matrix[0][0] << endl;
+
+
+
 
 	XLDocument doc;
 	doc.open("./Promzona.xlsx");
 	auto workbook = doc.workbook();
 	auto worksheet = doc.workbook().worksheet(worksheet_names[0]);
-
+	
 	int worksheets_count = doc.workbook().worksheetCount();
 	int columns_count = worksheet.columnCount();
 	int rows_count = worksheet.rowCount();
@@ -83,39 +89,19 @@ int main() {
 	
 	std::cout << ' ' << endl;
 
-	for (auto& row : worksheet.rows()) {
+	for (auto& row : worksheet.rows(1)) {
 		// for every ROW in SHEET...
 		
-		auto cell_range = row.cells(1, 1); // Read across the second COLUMN 
+		auto cell_range = row.cells(1, 1); // Read across the COLUMN 
 		for (auto& cell_value: cell_range) 
 		{
 			std::cout << cell_value.value().typeAsString() << " || " << cell_value.value() << endl;
 		}
 		
-		//for (auto& value : std::deque<XLCellValue>(row.values())) {
-			// for every VALUE in ROW...
-			
-			//std::cout << value.typeAsString() << " | " << value << endl;
-			
-			//if (!values_counter) 
-			//{
-			//	values_counter = 5;
-			//	break;
-			//}
-			//values_counter--;
-		//}
-		//if (!rows_counter)
-		//{ 
-		//	break;
-		//}
-		//rows_counter--;
-		//cout << endl;
 	}
 
-	//double cell_value = wks.cell("a2").value();
-	//cout << cell_value << endl;
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<seconds>(stop - start);
-	std::cout << "Seconds take to execute: "  << duration.count() << endl;
+	std::cout << "Took to execute: "  << duration.count() << " seconds." << endl;
 	return 0;
 }
