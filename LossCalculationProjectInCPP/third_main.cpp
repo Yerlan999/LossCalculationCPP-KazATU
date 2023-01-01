a6139184e3c203a37202fcab4515e12e43392d5d
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2023-01-01 22:17:58 +0600

    try 01

--- LossCalculationProjectInCPP/third_main.cpp
+++ LossCalculationProjectInCPP/third_main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
 #include <chrono>
-#include <sstream>
-#include <array>
-#include <typeinfo>
+#include <tuple>
 #include <complex>
 #include <Eigen/Dense>
 #include <OpenXLSX.hpp>
@@ -14,10 +10,29 @@ using namespace std;
 using namespace std::chrono;
 using namespace OpenXLSX;
 
-using Eigen::MatrixXcf;
+//using Eigen::MatrixXcf;
 using namespace Eigen;
 
-complex<float> mycomplex(10.0, -2.0);
+
+void insert_gap()
+{
+	std::cout << ' ' << endl;
+}
+
+void insert_start_separator()
+{
+	std::cout << ' ' << endl;
+	std::cout << "======================== *START* ========================" << endl;
+	std::cout << ' ' << endl;
+}
+
+void insert_end_separator()
+{
+	std::cout << ' ' << endl;
+	std::cout << "========================= *END* =========================" << endl;
+	std::cout << ' ' << endl;
+}
+
 
 // Моя функция для конвертации текста в цифру с плав. точкой
 float stringToFloat(string s)
@@ -37,12 +52,29 @@ int stringToInt(string s)
 	return toInt;
 }
 
+// Класс для составления диапазона для определенного присоединения
+class ProsoedRowRange 
+{
+public:
+	tuple<int, int> get_range(int which)
+	{
+		tuple <int, int> pris_range;
+
+		pris_range = make_tuple(10, 15);
+	}
+};
 
 //                         CT        DB        CT        DB        CT        DB 
 string worksheet_names[6] = {"Sheet1", "Sheet2", "Sheet3", "Sheet4", "Sheet5", "Sheet6"};
-int rows_counter = 1;
-int values_counter = 5;
 
+const int num_pris = 6;
+const int pris_num = 1; // 1 - only first, 2 - only second, 3 - only third, ... 0 - all of them
+int num_phases = 3; 
+int num_harms = 49; 
+int num_recs = 560; // number of recors (indexes) for one prisoed
+int titles_indexes[num_pris];
+int sheets_counter = 1;
+int titles_counter = 0;
 
 
 int main() {
@@ -50,20 +82,6 @@ int main() {
 	auto start = high_resolution_clock::now();
 
 	
-	complex<float> matrix[3][50][560] = {};
-	matrix[0][0][0] = mycomplex;
-
-
-	Matrix <complex<float>, 3, 3 > my_matrix;
-	my_matrix.setZero();
-	my_matrix(1, 2) = mycomplex;
-	
-	
-	std::cout << matrix[0][0] << endl;
-
-
-
-
 	XLDocument doc;
 	doc.open("./Promzona.xlsx");
 	auto workbook = doc.workbook();
@@ -72,33 +90,57 @@ int main() {
 	int worksheets_count = doc.workbook().worksheetCount();
 	int columns_count = worksheet.columnCount();
 	int rows_count = worksheet.rowCount();
+	
+	auto check_worksheet = worksheet;
 
+	insert_start_separator();
 
-	std::cout << ' ' << endl;
 	std::cout << "WorkSheets count: " << worksheets_count << endl;
-	std::cout << ' ' << endl;
-
-	for (auto& worksheetName : workbook.worksheetNames()) {
-		std::cout << "WorkSheet name: " << worksheetName << endl;
-	}
-
-	std::cout << ' ' << endl;
+	
+	insert_gap();
 	
 	std::cout << "Columns count: " << columns_count << endl;
 	std::cout << "Rows count: " << rows_count << endl;
 	
-	std::cout << ' ' << endl;
+	insert_gap();
 
-	for (auto& row : worksheet.rows(1)) {
-		// for every ROW in SHEET...
-		
-		auto cell_range = row.cells(1, 1); // Read across the COLUMN 
-		for (auto& cell_value: cell_range) 
+
+	// Определение начал данных измерении для каждого присоединения
+	for (auto& row : check_worksheet.rows())
+	{
+		auto cell_range = row.cells(1);
+		for (auto& cell_value : cell_range)
 		{
-			std::cout << cell_value.value().typeAsString() << " || " << cell_value.value() << endl;
+			bool is_title = cell_value.value().typeAsString() == "string";
+			if (is_title)
+			{
+				titles_indexes[titles_counter] = cell_value.cellReference().row();
+				titles_counter++;
+			}	
 		}
-		
 	}
+	titles_counter = 0;
+
+	insert_gap();
+	
+
+	// Testing
+	for (auto& worksheet_name : workbook.worksheetNames())
+	{
+		// for every SHEET... ["Sheet1", "Sheet2", "Sheet3", "Sheet4", "Sheet5", "Sheet6"]
+		auto worksheet = workbook.worksheet(worksheet_name);
+		//                         !custom range! Prisoed 
+		for (auto& row : worksheet.rows(2, 2))
+		{
+			auto cell_range = row.cells();
+			for (auto& cell_value : cell_range)
+			{
+				std::cout << "Worksheet: " << worksheet_name << " || Row: " << cell_value.cellReference().row() << " || Column: " << cell_value.cellReference().column() << " || Value: " << cell_value.value() << endl;
+			}
+		}
+	}
+
+	insert_end_separator();
 
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<seconds>(stop - start);
